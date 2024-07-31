4c706da5436303898fc1c5cb28443b58d9fb3fe0
sieun <charm516@naver.com>
2024-07-31 15:17:18 +0900

    [chore] - App.jsx 주석 추가

--- redux-st1/src/redux/modules/calculator.js
+++ redux-st1/src/redux/modules/calculator.js
@@ -1,8 +1,3 @@
-// 초기 상태값
-const initialState = {
-  number: 0,
-};
-
 // 액션 객체 type value 상수로 지정
 const PLUS_NUMBER = "PLUS_NUMBER";
 const MINUS_NUMBER = "MINUS_NUMBERs";
@@ -21,6 +16,11 @@ export const minusNumber = (value) => {
   };
 };
 
+// 초기 상태값
+const initialState = {
+  number: 0,
+};
+
 // 리듀서
 const calculator = (state = initialState, action) => {
   // action.payload 만큼 더하기
@@ -30,6 +30,7 @@ const calculator = (state = initialState, action) => {
     case PLUS_NUMBER:
       return {
         ...state,
+        // state.number (기존의 nubmer)에 action.paylaod(유저가 더하길 원하는 값)을 더한다.
         number: state.number + value,
       };
     case MINUS_NUMBER:
